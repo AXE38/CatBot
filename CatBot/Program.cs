68f633b064316bb435c270340f87d7bc980fe7f7
AXE <AXE@AXE.HOME>
2021-03-20 13:27:14 +0800

    Add project files.

---
+++ CatBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
