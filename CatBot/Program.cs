a7e69d4163144eefd226996348e9a7993f2b0e1f
Сергей Мельник <melnik.s.997@gmail.com>
2021-03-20 13:56:07 +0800

    added utils class

--- CatBot/Program.cs
+++ CatBot/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Configuration;
 
 namespace CatBot
 {
     class Program
     {
+        private static Configuration configManager = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        private static KeyValueConfigurationCollection confCollection = configManager.AppSettings.Settings;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var bot_token = confCollection["bot_token"].Value;
+            Utils.sConn = confCollection["connection_string"].Value;
+            Utils.catToken = confCollection["cat_token"].Value;
         }
     }
 }
